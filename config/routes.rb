a26b73d734c4f722ae0707486c3330b2381d0e0b
Rimen611 <rimenjenhani2026@u.northwestern.edu>
2026-04-27 16:12:49 -0500

    Add contact form modal with email sending

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :lost_items
   resources :found_items
+  
+  post "contacts/create_lost_item_contact", to: "contacts#create_lost_item_contact", as: :create_lost_item_contact
+  post "contacts/create_found_item_contact", to: "contacts#create_found_item_contact", as: :create_found_item_contact
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
