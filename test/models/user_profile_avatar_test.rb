9cb9d1c3d4861d297b6201cddfeaf7d8c1278560
Gwendolyn Slaughter <gwenny.slaughter@yahoo.com>
2026-06-08 08:51:51 -0500

    Added ability to choose initial for profile picture

--- test/models/user_profile_avatar_test.rb
+++ test/models/user_profile_avatar_test.rb
@@ -18,9 +18,14 @@ class UserProfileAvatarTest < ActiveSupport::TestCase
     assert_includes user.errors[:profile_avatar], "is not included in the list"
   end
 
-  test "allows blank profile avatar" do
-    user = users(:nu_student)
-    user.profile_avatar = nil
-    assert user.valid?
+  test "profile_avatar_initial is true for blank or initial selection" do
+    user = users(:admin)
+    assert user.profile_avatar_initial?
+
+    user.profile_avatar = "initial"
+    assert user.profile_avatar_initial?
+
+    user.profile_avatar = "cat"
+    assert_not user.profile_avatar_initial?
   end
 end
